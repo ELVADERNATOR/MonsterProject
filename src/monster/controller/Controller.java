e5efb1a88d16480ca50b1703596565f0978f6354
ELVADERNATOR <manufanmax@gmail.com>
2023-09-13 12:44:56 -0600

    new project + made default monster settings

---
+++ src/monster/controller/Controller.java
@@ -0,0 +1,17 @@
+package monster.controller;
+
+public class Controller
+{
+	
+	
+	public Controller()
+	{
+		
+		
+	}
+	
+	public void start()
+		{
+			
+		}
+}
---
+++ src/monster/model/MarshmallowMonster.java
@@ -0,0 +1,35 @@
+package monster.model;
+
+public class MarshmallowMonster
+{
+
+	
+	
+	
+	private String name = "Lenny";
+	private double armCount = 4.2;
+	private boolean hasTorso = true;
+	private boolean hasEye = true;
+	private boolean hasEyeSocket = true;
+	private int earCount = 3;
+	private int tailCount = 1;
+	private int legCount = 3;
+	private boolean isCute = true;
+	
+	
+	public MarshmallowMonster()
+	{
+		this.name = "Default name";
+		this.armCount = 2;
+		this.hasTorso = false;
+		this.hasEye = true;
+		this.hasEyeSocket = false;
+		this.earCount = 1;
+		this.tailCount = 0;
+		this.legCount = 2;
+		this.isCute = false;
+	}
+	
+	
+	
+}
